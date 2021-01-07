f8055c3dd2503d14f58847d9df5f710a28ac4f09
roy <roy4885@gmail.com>
2021-01-08 06:16:42 +0900

    add CDK app

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -2,6 +2,17 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { PipelineStack } from '../lib/pipeline-stack';
+import { LambdaStack } from '../lib/lambda-stack';
+
+const COMDECOMMIT_REPO_NAME = "pipeline";
 
 const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack');
+
+const lambdaStack = new LambdaStack(app, 'LambdaStack');
+new PipelineStack(app, 'PipelineDeployingLambdaStack', {
+    lambdaCode: lambdaStack.lambdaCode,
+    repoName: COMDECOMMIT_REPO_NAME
+});
+
+app.synth();
+
